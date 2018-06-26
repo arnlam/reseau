2905c19103f5b15b71cddbc09e483c9f4026e3fc
arn <arnaud.lambert@gmail.com>
2018-06-26 17:17:12 +0200

    graphql+mongoose

--- server/app.js
+++ server/app.js
@@ -4,11 +4,27 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
+
+
 const indexRouter = require('./routes/index');
 const apiRoutes = require('./routes/api');
 const app = express();
 const { graphqlExpress, graphiqlExpress } = require ('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+const typeDefs = require('./graphql/schema');
+const resolvers = require('./graphql/resolvers');
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost:27017/reseau';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const books = [
     {
@@ -20,19 +36,13 @@ const books = [
       author: 'Michael Crichton',
     },
   ];
-  const typeDefs = `
-    type Query { books: [Book] }
-    type Book { title: String, author: String }
-  `;
-  const resolvers = {
-    Query: { books: () => books },
-  };
-  const schema = makeExecutableSchema({
+
+const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
   });
 
-  
+
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
