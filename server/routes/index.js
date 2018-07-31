80a477d959d0d70ec33401de438edafc06f2e038
arn <arnaud.lambert@gmail.com>
2018-07-31 23:28:42 +0200

    deploiement

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'Vue' });
+  res.render(path.join(__dirname + '../front/dist/index.html'));
 });
 
 module.exports = router;
